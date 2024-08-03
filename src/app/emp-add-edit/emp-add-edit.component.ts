c36dffe3f646e7e35f7ef8cc6a0ca306ce05ebc3
moRezk12 <mohanedrazk54@gmail.com>
2024-08-03 15:48:57 +0300

    upload

---
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -0,0 +1,78 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { EmployeeService } from '../services/employee.service';
+import { CoreService } from '../core/core.service';
+
+@Component({
+  selector: 'app-emp-add-edit',
+  templateUrl: './emp-add-edit.component.html',
+  styleUrls: ['./emp-add-edit.component.scss']
+})
+export class EmpAddEditComponent implements OnInit {
+
+  education: string[] = [
+    'Matric',
+    'Diploma',
+    'Intermediate',
+    'Graduate',
+    'Post Graduate',
+  ];
+
+  empForm: FormGroup;
+
+  constructor(
+    private _fb: FormBuilder,
+    private _empService: EmployeeService,
+    private _dialogRef: MatDialogRef<EmpAddEditComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private _coreService: CoreService
+  ) {
+    this.empForm = this._fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      dob: '',
+      gender: '',
+      education: '',
+      company: '',
+      experience: '',
+      package: '',
+    });
+  }
+
+  ngOnInit(): void {
+    this.empForm.patchValue(this.data);
+  }
+
+  onFormSubmit(){
+    if (this.empForm.valid) {
+      if(this.data){
+        this._empService.updateEmployee( this.data.id , this.empForm.value).subscribe({
+          next: (val:any) => {
+            this._coreService.openSnackBar('Employee Updated!');
+            this._dialogRef.close(true);
+          },
+          error: (err:any) => {
+            console.log(err);
+
+          }
+        })
+      }
+      else
+      {
+        this._empService.addEmployee(this.empForm.value).subscribe({
+          next: (val:any) => {
+            this._coreService.openSnackBar('Employee Added Successfully');
+            this._dialogRef.close(true);
+          },
+          error: (err:any) => {
+            console.log(err);
+
+          }
+        })
+      }
+
+    }
+  }
+}
